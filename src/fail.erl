781f95213a582851cb50bcea657f315a5da373ee
Jonn Mostovoy <amarr.industrial@gmail.com>
2014-08-05 07:14:41 +0200

    Implement ping/0

--- src/fail.erl
+++ src/fail.erl
@@ -1,6 +1,8 @@
 -module(fail).
 -include_lib("riak_core/include/riak_core_vnode.hrl").
 
+-define(N, 5).
+
 -export([ ping/0
          ,start_session/2 % Starts a simple user/password session at fail_sessions_vnode
          ,get/2           % Gets a value under a key if the user/password tuple is correct
@@ -9,4 +11,12 @@
 
 %% api
 
+ping() ->
+  [{Node, _Type} | _] = preference_list({<<"ping">>, term_to_binary(os:timestamp())}, fail),
+  riak_core_vnode_master:sync_spawn_command(Node, ping, rts_vnode_master).
+
 %% internal functions
+
+preference_list(Key, Service) ->
+  DocIdx = riak_core_util:chash_key(Key, Service),
+  PreferenceList = riak_core_apl:get_apl(DocIdx, ?N, Service).
